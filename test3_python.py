d2076a5e1cc4d82127e8c3f4212545d441b6171a
Emily Wertheimer <71396046+Emily-Wertheimer@users.noreply.github.com>
2022-04-14 13:29:00 -0400

    Adding draft of python script for img viewing expt

---
+++ test3_python.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+
+
+##############################
+#Constants
+##############################
+"""
+def show_image(path):
+    """
+    The function receives a path to an image and show it on the screen
+
+    Parameters
+    ----------
+    path : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+    pass # We should google and learn how to do that
+    
+
+def run_experiment(stimuli_path:
+                   """
+    An experiment is defined by a list of stimuli that are presented with
+    a jittery interval beteen them, preceeded by a fixation point
+
+    Parameters
+    ----------
+    stimuli_path : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+    
+    ##############################
+#Constants
+FIXATION_IMAGE_PATH ''
